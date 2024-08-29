f97685af29a8ed015837fb22a9bca2df64f11fcc
Paige <paige.struthers@gmail.com>
2024-08-29 18:05:47 -0500

    added functionality to perform slide animation when in viewport

--- index.js
+++ index.js
@@ -57,3 +57,18 @@ document.addEventListener('DOMContentLoaded', () => {
         });
     });
 });
+
+document.addEventListener("DOMContentLoaded", () => {
+    const observer = new IntersectionObserver(entries => {
+        entries.forEach(entry => {
+            if (entry.isIntersecting) {
+                entry.target.classList.add('in-view');
+                return;
+            }
+        });
+    });
+
+    const allAnimatedElements = document.querySelectorAll('.animate');
+
+    allAnimatedElements.forEach((element) => observer.observe(element));
+});
